e562b5e254e45cda1d898e261cc7544258799494
Nania01 <148682556+Nania01@users.noreply.github.com>
2024-07-21 15:57:03 +1000

    Работа кнопок стилизации текста

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
 from QtMainWindow import Ui_color
 from QtSearchWindow import Ui_QtSearchWindow
 from QtReplaceWindow import Ui_QtReplaceWindow
 from QtStyles import Ui_Form
 from QtNewStyle import Ui_QtNewStyleWindow
+
 class MainWindow(QtWidgets.QMainWindow, Ui_color):
     def __init__(self):
         super().__init__()
@@ -15,6 +16,87 @@ class MainWindow(QtWidgets.QMainWindow, Ui_color):
         self.search.clicked.connect(self.open_search_window)
         self.replace.clicked.connect(self.open_replace_window)
         self.style.clicked.connect(self.open_style_window)
+
+        self.font.currentFontChanged.connect(self.update_font)
+        self.font_size.currentIndexChanged.connect(self.update_font_size)
+
+        self.bold.clicked.connect(self.toggle_bold)
+        self.italic.clicked.connect(self.toggle_italic)
+        self.underlined.clicked.connect(self.toggle_underlined)
+
+        self.update_font()
+        self.update_font_size()
+
+        self.bold_active = False
+        self.italic_active = False
+        self.underlined_active = False
+
+    def update_font(self, font=None):
+        if font is None:
+            font = self.font.currentFont()
+        self.apply_font(font)
+
+    def update_font_size(self):
+        size = int(self.font_size.currentText())
+        self.apply_font_size(size)
+
+    def apply_font(self, font):
+        current_font = self.text_edit.currentFont()
+        new_font = QtGui.QFont(font.family(), current_font.pointSize())
+        self.text_edit.setCurrentFont(new_font)
+
+    def apply_font_size(self, size):
+        current_font = self.text_edit.currentFont()
+        new_font = QtGui.QFont(current_font.family(), size)
+        self.text_edit.setCurrentFont(new_font)
+
+    def toggle_bold(self):
+        if self.bold_active:
+            self.bold.setStyleSheet('background-color: none')
+            self.bold_active = False
+            self.apply_font_bold(False)
+        else:
+            self.bold.setStyleSheet('background-color: lightblue')
+            self.bold_active = True
+            self.apply_font_bold(True)
+
+    def toggle_italic(self):
+        if self.italic_active:
+            self.italic.setStyleSheet('background-color: none')
+            self.italic_active = False
+            self.apply_font_italic(False)
+        else:
+            self.italic.setStyleSheet('background-color: lightblue')
+            self.italic_active = True
+            self.apply_font_italic(True)
+
+    def toggle_underlined(self):
+        if self.underlined_active:
+            self.underlined.setStyleSheet('background-color: none')
+            self.underlined_active = False
+            self.apply_font_underlined(False)
+        else:
+            self.underlined.setStyleSheet('background-color: lightblue')
+            self.underlined_active = True
+            self.apply_font_underlined(True)
+
+    def apply_font_bold(self, enabled):
+        current_font = self.text_edit.currentFont()
+        new_font = current_font
+        new_font.setBold(enabled)
+        self.text_edit.setCurrentFont(new_font)
+
+    def apply_font_italic(self, enabled):
+        current_font = self.text_edit.currentFont()
+        new_font = current_font
+        new_font.setItalic(enabled)
+        self.text_edit.setCurrentFont(new_font)
+
+    def apply_font_underlined(self, enabled):
+        current_font = self.text_edit.currentFont()
+        new_font = current_font
+        new_font.setUnderline(enabled)
+        self.text_edit.setCurrentFont(new_font)
 
     def open_search_window(self):
         self.search_window.show()
@@ -45,11 +127,11 @@ class StyleWindow(QtWidgets.QWidget, Ui_Form):
 
     def open_new_style_window(self):
         self.new_style_window.show()
+
 class NewStyleWindow(QtWidgets.QWidget, Ui_QtNewStyleWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
